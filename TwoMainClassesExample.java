07ecb1d88a900cdd544c3a1af512f5074d60640d
pradeept12 <pradeep.tallada@gmail.com>
2016-10-28 17:08:33 -0400

    Two Main classes in same file example

---
+++ TwoMainClassesExample.java
@@ -0,0 +1,25 @@
+/*class demo
+{
+int a=4;
+public void show()
+{
+System.out.println(a);
+}
+public static void main(String args[])
+{
+a++;
+show();
+}
+}*/
+class many {
+	public void main(String args[]) {
+		System.out.println("This is many class");
+	}
+}
+
+class TwoMainClassesExample {
+	public static void main(String args[]) {
+
+		System.out.println("This is manymain class");
+	}
+}
